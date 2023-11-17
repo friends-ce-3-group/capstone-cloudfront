66d1274465c31f981f222e55566e8413656f8aae
goodaccoustics <justinlim@JustinnoMacBook-Air.local>
2023-11-18 04:37:47 +0800

    initial commit

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "sctp-ce3-tfstate-bucket-1"
+    key = "friends-capstone-cloudfront.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2ß"
+}
